f349104c71587caf42650eed78c16eb0e04c7842
0xshodan <hngdean@gmail.com>
2023-06-18 12:33:16 +0300

    init

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "base_static",
     "users",
     "posts",
-    "main"
+    "main",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- src/main/views.py
+++ src/main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from typing import Any, Dict
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    # def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+    #     _context = super().get_context_data(**kwargs)
+    #     return _context
+    # def get(self, request: HttpRequest):
+    #     context_data = {"posts": posts}
+    #     template = loader.get_template("posts.html")
+    #     return HttpResponse(template.render(context_data, request))
